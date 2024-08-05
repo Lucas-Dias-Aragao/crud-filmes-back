ce158d93a1adcaa3d7a32be67fbf6a79436e8599
unknown <lucasd.aragao@hotmail.com>
2024-08-05 15:12:56 -0300

    corrige erro de cors

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
-const app = require('./src/app.js'); //tem express e routes
+// const app = require('./src/app.js'); //tem express e routes
+// const cors = require('cors');
+
+// const PORT = 3000;
+// //daqui pra cima
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+
+// //daqui pra baixo
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`)
+// })
+
+const app = require('./src/app.js');
 const cors = require('cors');
 
 const PORT = 3000;
-//daqui pra cima
 
-//daqui pra baixo
+// Configurar CORS
+const corsOptions = {
+  //origin: 'http://127.0.0.1:5500', // Substitua pelo domínio do seu front-end
+  origin: 'http://localhost:8002',
+  methods: 'GET,POST,PUT,DELETE',
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
-})
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+
 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
+// const express = require('express');
+// const routes = require('./routes');
+
+// const app = express();
+// routes(app);
+
+
+// module.exports = app;
+
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
+
+// Configurar CORS
+const corsOptions = {
+  //origin: 'http://127.0.0.1:5500', // Substitua pelo domínio do seu front-end
+  origin: 'http://localhost:8002',
+  methods: 'GET,POST,PUT,DELETE',
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+app.use(express.json()); // Para habilitar o parsing de JSON nas requisições
+
 routes(app);
 
 module.exports = app;
